6a48efb226850c52a6b18402ba2670fa7f77b1a7
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-01-13 22:17:19 -0500

    added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,38 +3,48 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-}
+  }
 
-type Thought {
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
     reactions: [Reaction]
-}
+  }
 
-type Reaction {
+  type Reaction {
     _id: ID
     reactionBody: String
     createdAt: String
     username: String
-}
+  }
 
-type Query {
+  type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
-}
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 // export the typeDefs
